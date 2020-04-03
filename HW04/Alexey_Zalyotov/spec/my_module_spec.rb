51c69f03aa38f183084e344559b049649f6b23cf
zal-lex <leshik89@gmail.com>
2020-04-03 21:18:37 +0300

    Change context messages in spec

--- HW04/Alexey_Zalyotov/spec/my_module_spec.rb
+++ HW04/Alexey_Zalyotov/spec/my_module_spec.rb
@@ -8,32 +8,53 @@ class Array
   include MyModule
 end
 
-RSpec.describe MyModule do
+describe '#my_each' do
   subject { array }
   let(:array) { Array.new([1, 2, 3]) }
-
-  describe '#my_each' do
-    context 'when no block is given' do
-      it 'returns an Enumerator' do
-        expect(array.my_each).to be_a(Enumerator)
-        # expect(array.my_each.to_a).to eq(array)
-      end
+  context 'when no block is given' do
+    it 'returns an Enumerator' do
+      expect(array.my_each).to be_a(Enumerator)
     end
   end
 
-  describe '#my_map' do
-    context 'when no block is given' do
-      it 'returns an Enumerator' do
-        expect(array.my_map).to be_a(Enumerator)
-      end
+  context 'when reciever is array [1, 2, 3]' do
+    it 'return modified elements' do
+      result = array.my_each { |element| puts element * 2 }
+      specify { expect(result).to output("2\n4\n6").to_stdout }
+    end
+  end
+end
+
+describe '#my_map' do
+  subject { array }
+  let(:array) { Array.new([1, 2, 3]) }
+  context 'when no block is given' do
+    it 'returns an Enumerator' do
+      expect(array.my_map).to be_a(Enumerator)
     end
   end
 
-  describe '#my_select' do
-    context 'when no block is given' do
-      it 'returns an Enumerator' do
-        expect(array.my_select).to be_a(Enumerator)
-      end
+  context 'when reciever is array [1, 2, 3]' do
+    it 'should return array' do
+      result = array.my_map { |element| element * 2 }
+      expect(result).to match_array [2, 4, 6]
+    end
+  end
+end
+
+describe '#my_select' do
+  subject { array }
+  let(:array) { Array.new([1, 2, 3]) }
+  context 'when no block is given' do
+    it 'returns an Enumerator' do
+      expect(array.my_select).to be_a(Enumerator)
+    end
+  end
+
+  context 'when reciever is array [1, 2, 3]' do
+    it 'should return array' do
+      result = array.my_select { |element| element < 3 }
+      expect(result).to match_array [1, 2]
     end
   end
 end
