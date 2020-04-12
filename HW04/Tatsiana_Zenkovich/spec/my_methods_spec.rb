7d00f2999d79cbaa4105ddaf50918747ca795d22
Vlad Hilko <vladislav.hilko@smartpension.co.uk>
2020-04-12 22:53:09 +0300

    Add rubocop-rspec (#92)

--- HW04/Tatsiana_Zenkovich/spec/my_methods_spec.rb
+++ HW04/Tatsiana_Zenkovich/spec/my_methods_spec.rb
@@ -1,84 +1,93 @@
-require 'my_methods.rb'
+require_relative 'my_methods.rb'
 # rubocop:disable Metrics/BlockLength
-describe '#my_each' do
-  subject { Array.new([1, 2, 3, 4, 5]) }
 
-  context 'when a block given' do
-    it 'calls the block passed to it exact number of times' do
-      expect do |block|
-        subject.my_each(&block)
-      end.to yield_control.exactly(5).times
+describe Array do
+  describe '#my_each' do
+    subject { described_class.new([1, 2, 3, 4, 5]) }
+
+    context 'when a block given' do
+      it 'calls the block passed to it exact number of times' do
+        expect do |block|
+          subject.my_each(&block)
+        end.to yield_control.exactly(5).times
+      end
+
+      it 'yields each element to the block' do
+        expect do |block|
+          subject.my_each(&block)
+        end.to yield_successive_args(1, 2, 3, 4, 5)
+      end
+
+      it 'returns the original array' do
+        expect(subject.my_each {}).to eq([1, 2, 3, 4, 5])
+      end
+
+      context 'when empty array is given' do
+        subject { described_class.new([]) }
+
+        it 'returns an empty array' do
+          expect(subject.my_each { |x| x + 3 }).to eq([])
+        end
+      end
     end
 
-    it 'yields each element to the block' do
-      expect do |block|
-        subject.my_each(&block)
-      end.to yield_successive_args(1, 2, 3, 4, 5)
+    context 'when no block given' do
+      it 'returns the Enumerator' do
+        expect(subject.my_each).to be_an_instance_of(Enumerator)
+      end
+    end
+  end
+  # rubocop:enable Metrics/BlockLength
+
+  describe '#my_map' do
+    subject { described_class.new(array).my_map { |x| x + 3 } }
+
+    let(:array) { [1, 2, 3, 4, 5] }
+
+    context 'when a block given' do
+      it { is_expected.to eq [4, 5, 6, 7, 8] }
+
+      it 'returns an array with same length' do
+        expect(array.my_map { |x| x + 3 }.length).to eq(array.length)
+      end
+
+      context 'when empty array is given' do
+        let(:array) { [] }
+
+        it { is_expected.to eq [] }
+      end
     end
 
-    it 'returns the original array' do
-      expect(subject.my_each {}).to eq([1, 2, 3, 4, 5])
-    end
-
-    context 'when empty array is given' do
-      subject { Array.new([]) }
-      it 'returns an empty array' do
-        expect(subject.my_each { |x| x + 3 }).to eq([])
+    context 'when no block given' do
+      it 'returns the Enumerator' do
+        expect(array.my_map).to be_an_instance_of(Enumerator)
       end
     end
   end
 
-  context 'when no block given' do
-    it 'returns the Enumerator' do
-      expect(subject.my_each).to be_an_instance_of(Enumerator)
-    end
-  end
-end
-# rubocop:enable Metrics/BlockLength
-describe '#my_map' do
-  subject { Array.new(array).my_map { |x| x + 3 } }
-  let(:array) { [1, 2, 3, 4, 5] }
+  describe '#my_select' do
+    subject { described_class.new(array).my_select { |x| x < 3 } }
 
-  context 'when a block given' do
-    it { is_expected.to eq [4, 5, 6, 7, 8] }
+    let(:array) { [1, 2, 3, 4, 5] }
 
-    it 'returns an array with same length' do
-      expect(array.my_map { |x| x + 3 }.length).to eq(array.length)
-    end
+    context 'when a block is given' do
+      it { is_expected.to eq [1, 2] }
 
-    context 'when empty array is given' do
-      let(:array) { [] }
-      it { is_expected.to eq [] }
-    end
-  end
+      it 'returns an array with at most the same length' do
+        expect(array.my_map { |x| x < 5 }.length).to be <= array.length
+      end
 
-  context 'when no block given' do
-    it 'returns the Enumerator' do
-      expect(array.my_map).to be_an_instance_of(Enumerator)
-    end
-  end
-end
+      context 'when empty array is given' do
+        let(:array) { [] }
 
-describe '#my_select' do
-  subject { Array.new(array).my_select { |x| x < 3 } }
-  let(:array) { [1, 2, 3, 4, 5] }
-
-  context 'when a block is given' do
-    it { is_expected.to eq [1, 2] }
-
-    it 'returns an array with at most the same length' do
-      expect(array.my_map { |x| x < 5 }.length).to be <= array.length
+        it { is_expected.to eq [] }
+      end
     end
 
-    context 'when empty array is given' do
-      let(:array) { [] }
-      it { is_expected.to eq [] }
-    end
-  end
-
-  context 'when no block given' do
-    it 'returns the Enumerator' do
-      expect(array.my_select).to be_an_instance_of(Enumerator)
+    context 'when no block given' do
+      it 'returns the Enumerator' do
+        expect(array.my_select).to be_an_instance_of(Enumerator)
+      end
     end
   end
 end
