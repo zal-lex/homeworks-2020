2f723b42a6828b307271b780d1be73148d292ed6
zal-lex <leshik89@gmail.com>
2020-04-13 22:20:21 +0300

    Change descriptions and refactor the code of tests

--- HW04/Alexey_Zalyotov/spec/my_module_spec.rb
+++ HW04/Alexey_Zalyotov/spec/my_module_spec.rb
@@ -5,174 +5,203 @@ class Array
 end
 
 # rubocop:disable Metrics/BlockLength
-RSpec.describe '#my_each' do
-  subject { Array.new([1, 2, 3]).my_each(&block) }
-  context 'when no block is given' do
-    let(:block) {}
-    context 'and reciever is array [1, 2, 3]' do
-      it 'returns an Enumerator' do
-        expect(subject).to be_a(Enumerator)
+RSpec.describe MyModule do
+  describe '#my_each' do
+    subject { Array.new(base_array).my_each(&block) }
+
+    context 'when no block is given' do
+      let(:block) {}
+
+      context 'when array is present' do
+        let(:base_array) { [1, 2, 3] }
+
+        it { is_expected.to be_a(Enumerator) }
+
+        describe '#to_a' do
+          it 'returns array [1, 2, 3]' do
+            expect(subject.to_a).to match_array [1, 2, 3]
+          end
+        end
       end
-      it '.to_a returns array [1, 2, 3]' do
-        expect(subject.to_a).to match_array [1, 2, 3]
+
+      context 'when array is empty' do
+        let(:base_array) { [] }
+
+        it { is_expected.to be_a(Enumerator) }
+
+        describe '#to_a' do
+          it 'returns an empty array' do
+            expect(subject.to_a).to match_array []
+          end
+        end
       end
     end
 
-    context 'or reciever is an empty array' do
-      subject { Array.new([]).my_each(&block) }
-      it 'returns an Enumerator' do
-        expect(subject).to be_a(Enumerator)
-      end
-      it '.to_a returns an empty array' do
-        expect(subject.to_a).to match_array []
-      end
-    end
-  end
+    context 'when the block { ->(a) { puts a } } is given' do
+      let(:block) { ->(a) { puts a } }
 
-  context 'when the block { ->(a) { puts a } } is given' do
-    let(:block) { ->(a) { puts a } }
-    context 'and reciever is array [1, 2, 3]' do
-      it 'puts elements in STDOUT and returns array [1, 2, 3]' do
-        expect { subject }.to output("1\n2\n3\n").to_stdout
-        expect(subject).to match_array [1, 2, 3]
-      end
-    end
+      context 'when array is present' do
+        let(:base_array) { [1, 2, 3] }
 
-    context 'or reciever is an empty array' do
-      subject { Array.new([]).my_each(&block) }
-      it 'returns an empty array' do
-        expect(subject).to match_array []
+        it 'puts elements in STDOUT and returns array [1, 2, 3]' do
+          expect { subject }.to output("1\n2\n3\n").to_stdout
+          expect(subject).to match_array [1, 2, 3]
+        end
       end
-    end
-  end
 
-  context 'when the block { ->(a) { puts a * a } } is given' do
-    let(:block) { ->(a) { puts a * a } }
-    context 'and reciever is array [1, 2, 3]' do
-      it 'puts changed elements in STDOUT and returns array [1, 2, 3]' do
-        expect { subject }.to output("1\n4\n9\n").to_stdout
-        expect(subject).to match_array [1, 2, 3]
+      context 'when array is empty' do
+        let(:base_array) { [] }
+
+        it { is_expected.to match_array [] }
       end
     end
 
-    context 'or reciever is an empty array' do
-      subject { Array.new([]).my_each(&block) }
-      it 'returns an empty array' do
-        expect(subject).to match_array []
-      end
-    end
-  end
-end
+    context 'when the block { ->(a) { puts a * a } } is given' do
+      let(:block) { ->(a) { puts a * a } }
 
-RSpec.describe '#my_map' do
-  subject { Array.new([1, 2, 3]).my_map(&block) }
-  context 'when no block is given' do
-    let(:block) {}
-    context 'and reciever is array [1, 2, 3]' do
-      it 'returns an Enumerator' do
-        expect(subject).to be_a(Enumerator)
-      end
-      it '.to_a returns array [1, 2, 3]' do
-        expect(subject.to_a).to match_array [1, 2, 3]
-      end
-    end
+      context 'when array is present' do
+        let(:base_array) { [1, 2, 3] }
 
-    context 'or reciever is an empty array' do
-      subject { Array.new([]).my_map(&block) }
-      it 'returns an Enumerator' do
-        expect(subject).to be_a(Enumerator)
+        it 'puts changed elements in STDOUT and returns array [1, 2, 3]' do
+          expect { subject }.to output("1\n4\n9\n").to_stdout
+          expect(subject).to match_array [1, 2, 3]
+        end
       end
-      it '.to_a returns an empty array' do
-        expect(subject.to_a).to match_array []
+
+      context 'when array is empty' do
+        let(:base_array) { [] }
+
+        it { is_expected.to match_array [] }
       end
     end
   end
 
-  context 'when the block { ->(a) { a * 2 } } is given' do
-    let(:block) { ->(a) { a * 2 } }
-    context 'and reciever is array [1, 2, 3]' do
-      it 'returns array [2, 4, 6]' do
-        expect(subject).to match_array [2, 4, 6]
+  describe '#my_map' do
+    subject { Array.new(base_array).my_map(&block) }
+
+    context 'when no block is given' do
+      let(:block) {}
+
+      context 'when array is present' do
+        let(:base_array) { [1, 2, 3] }
+
+        it { is_expected.to be_a(Enumerator) }
+
+        describe '#to_a' do
+          it 'returns array [1, 2, 3]' do
+            expect(subject.to_a).to match_array [1, 2, 3]
+          end
+        end
+      end
+
+      context 'when array is empty' do
+        let(:base_array) { [] }
+
+        it { is_expected.to be_a(Enumerator) }
+
+        describe '#to_a' do
+          it 'returns an empty array' do
+            expect(subject.to_a).to match_array []
+          end
+        end
       end
     end
 
-    context 'or reciever is an empty array' do
-      subject { Array.new([]).my_map(&block) }
-      it 'returns an empty array' do
-        expect(subject).to match_array []
-      end
-    end
-  end
+    context 'when the block { ->(a) { a * 2 } } is given' do
+      let(:block) { ->(a) { a * 2 } }
 
-  context 'when the block { ->(a) { a.odd? } is given' do
-    let(:block) { ->(a) { a.odd? } }
-    context 'and reciever is array [1, 2, 3]' do
-      it 'returns array [true, false, true]' do
-        expect(subject).to match_array [true, false, true]
-      end
-    end
+      context 'when array is present' do
+        let(:base_array) { [1, 2, 3] }
 
-    context 'or reciever is an empty array' do
-      subject { Array.new([]).my_map(&block) }
-      it 'returns an empty array' do
-        expect(subject).to match_array []
+        it 'returns array [2, 4, 6]' do
+          expect(subject).to match_array [2, 4, 6]
+        end
       end
-    end
-  end
-end
 
-RSpec.describe '#my_select' do
-  subject { Array.new([1, 2, 3]).my_select(&block) }
-  context 'when no block is given' do
-    let(:block) {}
-    context 'and reciever is array [1, 2, 3]' do
-      it 'returns an Enumerator' do
-        expect(subject).to be_a(Enumerator)
-      end
-      it '.to_a returns array [1, 2, 3]' do
-        expect(subject.to_a).to match_array [1, 2, 3]
+      context 'when array is empty' do
+        let(:base_array) { [] }
+
+        it { is_expected.to match_array [] }
       end
     end
 
-    context 'or reciever is an empty array' do
-      subject { Array.new([]).my_select(&block) }
-      it 'returns an Enumerator' do
-        expect(subject).to be_a(Enumerator)
-      end
-      it '.to_a returns an empty array' do
-        expect(subject.to_a).to match_array []
-      end
-    end
-  end
+    context 'when the block { ->(a) { a.odd? } is given' do
+      let(:block) { ->(a) { a.odd? } }
 
-  context 'when the block { ->(a) { a < 3 } } is given' do
-    let(:block) { ->(a) { a < 3 } }
-    context 'and reciever is array [1, 2, 3]' do
-      it 'returns array [1, 2]' do
-        expect(subject).to match_array [1, 2]
-      end
-    end
+      context 'when array is present' do
+        let(:base_array) { [1, 2, 3] }
 
-    context 'or reciever is an empty array' do
-      subject { Array.new([]).my_select(&block) }
-      it 'returns an empty array' do
-        expect(subject).to match_array []
+        it { is_expected.to match_array [true, false, true] }
+      end
+
+      context 'when array is empty' do
+        let(:base_array) { [] }
+
+        it { is_expected.to match_array [] }
       end
     end
   end
 
-  context 'when the block { ->(a) { a.odd? } } is given' do
-    let(:block) { ->(a) { a.odd? } }
-    context 'and reciever is array [1, 2, 3]' do
-      it 'returns array [1, 3]' do
-        expect(subject).to match_array [1, 3]
+  describe '#my_select' do
+    subject { Array.new(base_array).my_select(&block) }
+
+    context 'when no block is given' do
+      let(:block) {}
+
+      context 'when array is present' do
+        let(:base_array) { [1, 2, 3] }
+
+        it { is_expected.to be_a(Enumerator) }
+
+        describe '#to_a' do
+          it 'returns array [1, 2, 3]' do
+            expect(subject.to_a).to match_array [1, 2, 3]
+          end
+        end
+      end
+
+      context 'when array is empty' do
+        let(:base_array) { [] }
+
+        it { is_expected.to be_a(Enumerator) }
+
+        describe '#to_a' do
+          it 'returns an empty array' do
+            expect(subject.to_a).to match_array []
+          end
+        end
       end
     end
 
-    context 'or reciever is an empty array' do
-      subject { Array.new([]).my_select(&block) }
-      it 'returns an empty array' do
-        expect(subject).to match_array []
+    context 'when the block { ->(a) { a < 3 } } is given' do
+      let(:block) { ->(a) { a < 3 } }
+
+      context 'when array is present' do
+        let(:base_array) { [1, 2, 3] }
+
+        it { is_expected.to match_array [1, 2] }
+      end
+
+      context 'when array is empty' do
+        let(:base_array) { [] }
+
+        it { is_expected.to match_array [] }
+      end
+    end
+
+    context 'when the block { ->(a) { a.odd? } } is given' do
+      let(:block) { ->(a) { a.odd? } }
+
+      context 'when array is present' do
+        let(:base_array) { [1, 2, 3] }
+
+        it { is_expected.to match_array [1, 3] }
+      end
+
+      context 'when array is empty' do
+        let(:base_array) { [] }
+
+        it { is_expected.to match_array [] }
       end
     end
   end
